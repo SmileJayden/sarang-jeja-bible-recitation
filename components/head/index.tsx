2ee553df7e20a97c895772a9ea6ce4e6dae3f7d4
SmileJayden <wkdwodud07@gmail.com>
2021-07-23 12:10:36 +0900

    init pwa

--- components/head/index.tsx
+++ components/head/index.tsx
@@ -1,27 +1,30 @@
 import Head from "next/head";
 
+const APP_NAME = "드리미 제자훈련";
+const APP_DESCRIPTION = "드리미 제자훈련 암송시험 말씀모음, 모의시험";
+
 function HeadComp() {
   return (
     <Head>
-      <title>제자훈련 암송시험</title>
+      <title>{APP_NAME}</title>
       <meta
-        name="description"
-        content="사랑의교회 제자훈련 암송시험 말씀모음, 예비시험"
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
       />
-      <link rel="icon" href="/holy-bible-256.ico" />
+      <meta name="description" content={APP_DESCRIPTION} />
       <meta name="title" content="AITRICS" />
-
       <meta property="og:type" content="website" />
       <meta
         property="og:url"
         content="https://sarang-jeja-bible-recitation.vercel.app/"
       />
-      <meta property="og:title" content="제자훈련 말씀암송" />
-      <meta
-        property="og:description"
-        content={"사랑의교회 제자훈련 암송시험 말씀모음, 모의시험"}
-      />
-      <meta property="og:image" content="/bible-study.jpeg" />
+      <meta property="og:title" content={APP_NAME} />
+      <meta property="og:description" content={APP_DESCRIPTION} />
+      <meta property="og:image" content="/bible.png" />
+      <meta name="theme-color" content="#FFFFFF" />
+      <link rel="shortcut icon" href="/bible.png" />
+      <link rel="apple-touch-icon" href="/icon/maskable_icon.png" />
+      <link rel="manifest" href="/manifest.json" />
     </Head>
   );
 }
